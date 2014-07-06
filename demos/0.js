26844d0b41b64133301c11b3203e3a08453dce77
allen <wongguang.wg@alibaba-inc.com>
2014-07-06 16:06:21 +0800

    demo done

--- demos/0.js
+++ demos/0.js
@@ -7,8 +7,12 @@ var Mustache = require("mustache");
 var cases = [
     {
         rule: ':has(:root > .TYPE:val("Call")) > .expression:has(:root > .TYPE:val("Dot")):has( :root > .expression > .name:val("S") ,  :root > .expression > .name:val("KISSY") ):has(:root > .property:val("namespace"))',
-        message: "命中规则 S.namespace ！在第{{start.line}}行的第{{start.col}}个字符。"
-    }
+        message: "命中规则 [ S.namespace ] 在第 {{start.line}} 行的第 {{start.col}} 个字符。"
+    },
+    {
+        rule: ':has(:root > .TYPE:val("Call")) > .expression:has(:root > .TYPE:val("Dot")):has( :root > .expression > .name:val("S") ,  :root > .expression > .name:val("KISSY") ):has(:root > .property:val("config"))',
+        message: "命中规则 [ S.config ] 在第 {{start.line}} 行的第 {{start.col}} 个字符。"
+    },
 ]
 
 function getAst(codeStr) {
@@ -20,12 +24,12 @@ function analyze(codeStr) {
     var ast = getAst(codeStr);
     var ret = [];
 
-    cases.forEach(function(oneCase){
+    cases.forEach(function (oneCase) {
         var rule = oneCase.rule;
         var message = oneCase.message;
         var result = JSONSelect.match(rule, ast);
-        if(result.length){
-            result.forEach(function(item){
+        if (result.length) {
+            result.forEach(function (item) {
                 ret.push(Mustache.render(message, item));
             });
         }
@@ -38,9 +42,11 @@ function analyze(codeStr) {
 ///// Boot
 
 var result = analyze([
-    '(1+1), S.namespace("xxx", {});',
-    'KISSY.namespace("xxx", {});',
-    'S.config({});'
+    "KISSY.add(function(){",
+    '    (1+1), S.namespace("xxx", {});',
+    '    KISSY.namespace("xxx", {});',
+    '    S.config({});',
+    "});"
 ].join("\n"));
 
 console.log(result.join("\n"));
